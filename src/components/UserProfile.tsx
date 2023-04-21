ca0cff47495f4688a9c3abbd05df85b8a512ccce
tokkissi <alal4674@gmail.com>
2023-04-21 23:08:45 +0900

    feat: add user profile in user page

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,45 @@
+import { ProfileUser } from "@/model/user";
+import Avatar from "./ui/Avatar";
+import FollowButton from "./FollowButton";
+
+type Props = {
+  user: ProfileUser;
+};
+
+export default function UserProfile({ user }: Props) {
+  const { image, name, username, followers, following, posts } = user;
+  const info = [
+    {
+      title: "posts",
+      data: posts,
+    },
+    {
+      title: "followers",
+      data: followers,
+    },
+    {
+      title: "following",
+      data: following,
+    },
+  ];
+  return (
+    <section className="w-full flex flex-col md:flex-row justify-center items-center py-12 border-b border-neutral-300">
+      <Avatar image={image} size="xlarge" highlight />
+      <div className="md:ml-10 basis-1/3">
+        <div className="flex flex-col items-center md:flex-row">
+          <h1 className="text-2xl md:mr-8 my-2 md:my-0 ">{username}</h1>
+          <FollowButton user={user} />
+        </div>
+        <ul className="my-4 flex gap-4">
+          {info.map(({ title, data }, index) => (
+            <li key={index}>
+              <span className="font-bold mr-1">{data}</span>
+              {title}
+            </li>
+          ))}
+        </ul>
+        <p className="text-xl font-bold text-center md:text-start">{name}</p>
+      </div>
+    </section>
+  );
+}
