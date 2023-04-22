784e3821606c5043607c53767d9d38485d2f8ffb
tokkissi <alal4674@gmail.com>
2023-04-22 18:18:24 +0900

    feat: add metadata

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,6 +1,9 @@
+import UserPosts from "@/components/UserPosts";
 import UserProfile from "@/components/UserProfile";
 import { getUserForProfile } from "@/service/user";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 
 type Props = {
   params: {
@@ -8,12 +11,29 @@ type Props = {
   };
 };
 
+const getUser = cache(async (username: string) => getUserForProfile(username));
+
 export default async function UserPage({ params: { username } }: Props) {
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
 
   if (!user) {
     notFound();
   }
 
-  return <UserProfile user={user} />;
+  return (
+    <section className="w-full">
+      <UserProfile user={user} />
+      <UserPosts user={user} />
+    </section>
+  );
+}
+
+export async function generateMetadata({
+  params: { username },
+}: Props): Promise<Metadata> {
+  const user = await getUser(username);
+  return {
+    title: `${user?.name} (@${username}) · Instantgram Photos`,
+    description: `${user?.name}'s all Instantgram posts`,
+  };
 }
