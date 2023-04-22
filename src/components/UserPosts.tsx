14fd6cc9db97f03b20abf9583cf67e9f350facd1
tokkissi <alal4674@gmail.com>
2023-04-22 18:17:23 +0900

    feat: add user posts in user page

---
+++ src/components/UserPosts.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { ProfileUser } from "@/model/user";
+import { useState } from "react";
+import PostIcon from "./ui/icons/PostIcon";
+import HeartIcon from "./ui/icons/HeartIcon";
+import BookmarkIcon from "./ui/icons/BookmarkIcon";
+import PostGrid from "./PostGrid";
+
+type Props = {
+  user: ProfileUser;
+};
+
+const tabs = [
+  {
+    type: "posts",
+    icon: <PostIcon />,
+  },
+  {
+    type: "liked",
+    icon: <HeartIcon className={"w-3 h-3"} />,
+  },
+  {
+    type: "saved",
+    icon: <BookmarkIcon className={"w-3 h-3"} />,
+  },
+];
+
+const UserPosts = ({ user: { username } }: Props) => {
+  const [query, setQuery] = useState(tabs[0].type);
+
+  return (
+    <section>
+      <ul className="flex justify-center uppercase">
+        {tabs.map(({ type, icon }) => (
+          <li
+            className={`flex items-center mx-12 p-4 cursor-pointer border-black ${
+              type === query && "font-bold border-t"
+            }`}
+            key={type}
+            onClick={() => setQuery(type)}
+          >
+            <button className="scale-150 md:scale-100">{icon}</button>
+            <span className="ml-1 hidden md:inline">{type}</span>
+          </li>
+        ))}
+      </ul>
+      <PostGrid username={username} query={query} />
+    </section>
+  );
+};
+
+export default UserPosts;
---
+++ src/components/ui/icons/PostIcon.tsx
@@ -0,0 +1,5 @@
+import { RiFileList2Line } from "react-icons/ri";
+
+export default function PostIcon() {
+  return <RiFileList2Line />;
+}
