f5524ff3600793501e945a3a3d3a57520cae10fa
tokkissi <alal4674@gmail.com>
2023-04-26 17:19:48 +0900

    feat: add appending comment API

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -16,7 +16,8 @@ export default function SideBar({ user: { name, username, image } }: Props) {
         </div>
       </div>
       <p className="text-sm text-neutral-500 mt-8">
-        About · Help ·Press · API · Jobs · Privacy · Terms · Location · Language
+        About · Help · Press · API · Jobs · Privacy · Terms · Location ·
+        Language
       </p>
       <p className="font-bold text-sm text-neutral-500 mt-8">
         ⒸCopyright INSTANTGRAM from METAL
